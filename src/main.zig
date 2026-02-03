8e8e8aeb02f71c5839e8efe2decafbfada7e8395
brandtall <webyahyaver@gmail.com>
2026-02-03 09:18:06 +0400

    fix REPL

--- src/main.zig
+++ src/main.zig
@@ -2,11 +2,10 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 pub const InputBuffer = struct {
-
     buffer: []u8,
     input_length: usize,
 
-    pub fn init(allocator: Allocator) !*InputBuffer{
+    pub fn init(allocator: Allocator) !*InputBuffer {
         const self = try allocator.create(InputBuffer);
         const backing_memory = try allocator.alloc(u8, 4096);
         self.* = .{
@@ -17,14 +16,13 @@ pub const InputBuffer = struct {
         return self;
     }
 
-    pub fn deinit(self: *InputBuffer, allocator: Allocator) void{
+    pub fn deinit(self: *InputBuffer, allocator: Allocator) void {
         allocator.free(self.buffer);
         allocator.destroy(self);
     }
-
 };
 
-const InputError = error {
+const InputError = error{
     ErrorBuffer,
     BufferOverflow,
 };
@@ -37,10 +35,17 @@ pub fn main() !void {
 
     const inputBuffer = try InputBuffer.init(allocator);
 
-    while(true) {
+    while (true) {
         printPrompt();
         try readInput(inputBuffer);
-    }    
+        const command = inputBuffer.buffer[0..inputBuffer.input_length];
+        if (std.mem.eql(u8, command, ".exit")) {
+            inputBuffer.deinit(allocator);
+            break;
+        } else {
+            std.debug.print("Unrecogonized command '{s}'. \n", .{command});
+        }
+    }
 }
 
 pub fn printPrompt() void {
@@ -48,20 +53,10 @@ pub fn printPrompt() void {
 }
 
 pub fn readInput(inputBuffer: *InputBuffer) !void {
-
     var stdin_buf: [4096]u8 = undefined;
     var stdin_impl = std.fs.File.stdin().reader(&stdin_buf);
-    while (stdin_impl.takeByte()) |char| {
-        
-    if (try stdin_impl.readUntilDelimiterOrEof(inputBuffer.buffer, '\n')) |line| {
-        if (line.len > 0 and line[line.len - 1] == '\r') {
-             inputBuffer.input_length = line.len - 1;
-        } else {
-             inputBuffer.input_length = line.len;
-        }
+    const stdin: *std.io.Reader = &stdin_impl.interface;
+    const line = try stdin.takeDelimiterExclusive('\n');
+    inputBuffer.input_length = line.len;
+    @memcpy(inputBuffer.buffer[0..inputBuffer.input_length], line[0..inputBuffer.input_length]);
     }
-    else {
-    }
-}
-
-
